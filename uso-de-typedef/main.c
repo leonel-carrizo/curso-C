bf5125f6ddd39c290f9b221f8485983cdeaff897
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-12-01 15:21:55 +0000

    typedef como alias de tipos de datos

---
+++ uso-de-typedef/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int valor;
+
+    typedef int entero;
+
+    entero valor1;
+    entero valor2;
+
+    typedef long entero_largo;
+
+    entero_largo valor3;
+}
