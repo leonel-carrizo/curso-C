544f4ed766070206b145094f15e1c40fda27a957
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-12-02 10:54:33 +0000

    uso de tipo de dato enum

--- tipos-de-datos/enums.c
+++ tipos-de-datos/enums.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
+enum weekDays
 {
-    printf("\n");
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+};
 
+int main()
+{
+    enum weekDays today;
+    today = Saturday;
+
+    printf("Day %d\n", today+1);
+
+    enum deck
+    {
+        corazones = 1,
+        azotes = 5,
+        espaada = 10,
+        oro = 15,
+    } cartas;
+
+    cartas = oro;
+
+        printf("Card Value %d\n", cartas);
 
 
     return 0;
