42a44afe376f0b25e8135a710d0af8df13636429
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-12-02 17:06:38 +0000

    Tipo de dato char

--- tipos-de-datos/char.c
+++ tipos-de-datos/char.c
@@ -1,4 +1,6 @@
-/*Tipo de Dato char: se usa en variables que van a servir para almacenar un caracter.
-        char 1 Byte -120 to 127
-        unsigned char 1 Byte 0 to 255
+/*Tipo de Dato char: se usa en variables que van a servir para almacenar un caracter. Esta compuesto de igual forma por un rango de valores como los int de tipo entero.
+
+        char: 1 Byte -> -128 to 127
+        unsigned char: 1 Byte -> 0 to 255
+
 */
